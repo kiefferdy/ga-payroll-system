2aa041d5e260effe77f0e25b6521758bdad438d6
ReddMA <r3ddaustria@gmail.com>
2023-10-23 13:43:03 +0800

    create otp design

---
+++ pages/otp.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="flex flex-col items-center">
+      <h1 class="text-xl font-bold mt-20">Enter Code</h1>
+      <p>OTP</p>
+      <div class="flex justify-center space-x-2 m-5">
+         <div class="otp-line"></div>
+         <div class="otp-line"></div>
+         <div class="otp-line"></div>
+         <div class="otp-line"></div>
+         <div class="otp-line"></div>
+         <div class="otp-line"></div>
+      </div>
+      
+   </div>
+</template>
+
+<style>
+  .otp-line {
+    width: 2em; 
+    height: 2px;
+    background: #689E6E; 
+    margin: 0 0.2em;
+  }
+</style>
