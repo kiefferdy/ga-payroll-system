93773a66fd27654216da25677f14ec98ca8c1a21
ReignLarraquel <reignlarraquel@gmail.com>
2023-10-24 23:54:04 +0800

    Modify index

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,19 @@
 <template>
-   <!-- 
-      TODO:
-      implement preloading
-    -->
-   <div class="flex flex-row place-content-end text-xs mt-2">
-      <p class="mr-4">Status: </p>
-      <div class="bg-clock_out_red rounded-full w-2.5 h-2.5 mx-1 mt-0.5"></div>
-      <p> Out</p>
-   </div>
-   <div class="flex flex-col items-center">
-      <h1 class="text-xl font-bold mt-20">Good Evening,</h1> <!-- TODO: Greeting should be based on time-->
+   <div class="card text-black flex items-center justify-center h-[30rem] w-[60rem]">
+      <div class="flex flex-row self-end">
+         <p class="mr-4 ">Status:</p>
+         <div class="bg-clock_out_red rounded-full w-2.5 h-2.5 mx-1 mt-2"></div>
+         <p>Out</p>
+      </div>
+      <h1 class="card-title text-2xl mt-24">Good Morning,</h1>
       <p>Reign</p>
-      <p class="mt-5 mb-1.5">9:00 AM</p> <!-- TODO: Display the real time clock-->
-      <button class="w-24 h-24 bg-clock_in_green text-white rounded-full flex items-center justify-center"> Time In</button> <!-- TODO: Should show otp page -->
-   </div>
-   <div class="flex place-content-end text-xs mt-20 mb-2">
-      <button class="font-bold flex flex-row"> Logout <img class="mx-2 mt-0.5 w-3 h-3" src="~/assets/icons/exit.png"></button> <!-- TODO: Logout user and redirect to login page-->
+      <p class="mt-5 mb-1.5">9:00 AM</p>
+      <div class="card-actions">
+         <button class="btn btn-circle btn-ghost w-36 h-36 mt-1 bg-clock_in_green text-white"> <NuxtLink to="/otp">Time In</NuxtLink></button>
+      </div>
+      <div class="card-actions self-end mt-24">
+      <button class="font-bold btn btn-sm btn-ghost btn-circle w-32 ">Logout<img class="mx-2 w-4 h-4" src="~/assets/icons/exit.png"></button>
+      </div>
    </div>
 </template>
 
