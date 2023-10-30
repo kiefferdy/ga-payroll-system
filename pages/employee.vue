4e2f2d65a527790bec13ab1a6517885c6ad81215
ReignLarraquel <reignlarraquel@gmail.com>
2023-10-31 03:13:35 +0800

    Modify employee

--- pages/employee.vue
+++ pages/employee.vue
@@ -4,10 +4,10 @@
          <div class="card card-side  mx-10 mt-5 justify-between">
             <div class="join">
                <input type="text" placeholder="Search..." class="input input-sm bg-primary_white border-search_stroke_gray rounded rounded-l-full w-60"/>
-               <button class="btn btn-sm btn-ghost bg-button_green w-10 rounded rounded-r-full btn-round"><img src="~/assets/icons/search.png"></button>
+               <button class="btn btn-sm btn-ghost bg-dark_green w-10 rounded rounded-r-full btn-round"><img src="~/assets/icons/search.png"></button>
             </div>
             <div>
-               <NuxtLink><button class="btn btn-sm bg-button_green btn-ghost text-white rounded-full capitalize"><img src="~/assets/icons/add.png" class="w-4">Add Account</button></NuxtLink>
+               <NuxtLink to="/create-account"><button class="btn btn-sm bg-dark_green btn-ghost text-white rounded-full capitalize"><img src="~/assets/icons/add.png" class="w-4">Add Account</button></NuxtLink>
             </div> 
          </div>
          <div>
@@ -17,10 +17,15 @@
             <ProfileCard/>
          </div>
       </div>
-      <ul class="menu w-[10rem] p-0 font-bold text-white">
-         <li class="active bg-primary_white rounded-r-[1rem] py-2 items-center text-black"><NuxtLink to="/employees">Employees</NuxtLink></li>
-         <li class="py-2 items-center"><NuxtLink>Records</NuxtLink></li>
-         <li class="py-2 items-center"><NuxtLink>Settings</NuxtLink></li>
+      <ul class="menu w-[10rem] p-0 font-bold text-white justify-between">
+         <div>
+            <li class="active bg-primary_white rounded-r-[1rem] py-2 items-center text-black"><NuxtLink to="/employees">Employees</NuxtLink></li>
+            <li class="py-2 items-center"><NuxtLink>Records</NuxtLink></li>
+            <li class="py-2 items-center"><NuxtLink>Settings</NuxtLink></li>         
+         </div>
+         <div class="self-end mb-1">
+            <button class="font-bold btn btn-sm btn-ghost btn-circle w-28">Logout<img class="mx-2 w-4 h-4" src="~/assets/icons/exit_white.png"></button>
+         </div>
       </ul>
    </div>
 </template>
