3123d7c267481bbe476513d4aea1fe2316d7f49a
ReignLarraquel <reignlarraquel@gmail.com>
2023-10-31 03:14:13 +0800

    Add create account

---
+++ pages/create-account.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="card text-black flex justify-center h-[30rem] w-[35rem]">
+      <h1 class="card-title">Create Account</h1>
+      <div class="card card-side justify-between">
+         <div>
+            <label class="label-text text-black mt-4">First Name<br></label>
+            <input type="text" class="input input-sm border-primary_green bg-primary_white rounded w-fit">
+         </div>
+         <div>
+            <label class="label-text text-black mt-4">Last Name<br></label>
+            <input type="text" class="input input-sm border-primary_green bg-primary_white rounded w-full">
+         </div>
+      </div>
+   </div>
+</template>
