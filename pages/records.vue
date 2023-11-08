a38db9ef115a5398f2e52cd082663964c55e1e65
ReignLarraquel <reignlarraquel@gmail.com>
2023-11-09 01:13:48 +0800

    Fix notes design

--- pages/records.vue
+++ pages/records.vue
@@ -1,7 +1,7 @@
 <template>
       <div class="card card-side h-[37rem] w-[65rem] bg-dark_green text-black">
-      <div class="card h-[37rem] w-[55rem] bg-primary_white rounded rounded-[1rem] rounded-br-[1rem]">
-         <div class="card card-side justify-between mx-10 mt-3">
+      <div class="card h-[37rem] w-[55rem] bg-primary_white rounded-[1rem] rounded-br-[1rem]">
+         <div class="card card-side justify-between mx-10 mt-5">
             <div class="card card-side">
                <p class="pe-2 mt-1">Employee:</p>
                <div class="dropdown dropdown-hover w-56 border-button_green border-2 rounded-md">
@@ -25,9 +25,9 @@
                </div>
             </div>
          </div>
-         <div class="card mx-5 my-2 bg-off_white p-2 px-5">
-            <table class="table text-xs">
-               <thead class="">
+         <div class="card mx-5 my-5 bg-off_white p-2 px-5">
+            <table class="table table-sm">
+               <thead class="text-black">
                   <tr>
                      <th class="me-5"></th>
                      <th>in</th>
@@ -132,40 +132,47 @@
                </tbody>
             </table>
          </div>
-         <div class="card card-side justify-between mx-5">
-            <div>
-               <div class="text-sm card card-side justify-between bg-off_white px-5 py-2">
-                  <p class="pe-36">Pay per Hr</p>
-                  <p class="pe-36">Pay per Min</p>               
+         <div class="card card-side justify-between mx-5 text-sm">
+            <div class="card bg-off_white px-5 py-2">
+               <div class="card card-side justify-between pb-4 pt-1">
+                  <div class="pe-36 card card-side">
+                     <p class="pe-2 font-bold">Pay per Hr:</p>
+                     <p>36.50</p> 
+                  </div>
+                  <div class="pe-36 card card-side">
+                     <p class="pe-2 font-bold">Pay per Min:</p>
+                     <p>43.50</p>
+                  </div>        
                </div>
-               
+               <p class="font-bold text-black">Additional Notes:</p>
+               <input type="text" class="w-full h-14 rounded-md bg-off_white border-2 border-dark_green"/>   
             </div>
             <div class="card px-5 py-2 bg-off_white">
-               <table class="text-left text-xs">
+               <table class="text-xs">
                   <tbody>
                      <tr>
-                        <th class="font-normal">Weekly Total</th>
-                        <td class="w-16 border-b-2 border-dark_green"></td>
+                        <th class="font-normal text-left">Weekly Total</th>
+                        <td class="w-16 text-right">4432.60</td>
                      </tr>
                      <tr>
-                        <th class="font-normal">Friday</th>
-                        <td class="w-16 border-b-2 border-dark_green"></td>
+                        <th class="font-normal text-left">Friday</th>
+                        <td class="w-16 text-right">1000</td>
                      </tr>
                      <tr>
-                        <th class="font-normal">Bale</th>
-                        <td class="w-16 border-b-2 border-dark_green"></td>
+                        <th class="font-normal text-left">Bale</th>
+                        <td class="w-16 text-right">200</td>
                      </tr>
                      <tr>
-                        <th class="font-normal">Sat Advance</th>
-                        <td class="w-16 border-b-2 border-dark_green"></td>
+                        <th class="font-normal text-left">Sat Advance</th>
+                        <td class="w-16 text-right"></td>
                      </tr>
-                     <tr>
-                        <th class="font-normal">Deduction</th>
-                        <td class="w-16 border-b-2 border-dark_green"></td>
+                     <tr class="border-b-2 border-dark_green">
+                        <th class="font-normal text-left">Deduction</th>
+                        <td class="w-16 text-right">300</td>
                      </tr>
                      <tr class="text-base">
-                        <th class="pe-10 pt-2">Total Balance</th>
-                        <td class="w-16 border-b-2 border-dark_green ms-20"></td>
+                        <th class="pe-10 pt-2 text-left">Total Balance</th>
+                        <td class="w-16 pt-2 text-right">2932.60</td>
                      </tr>
                   </tbody>
                </table>
