4d61634a6bf4187c87775eff01dcd01546e3a7c5
ReddMA <r3ddaustria@gmail.com>
2023-11-10 19:02:35 +0800

    Modify create account

--- pages/create-account.vue
+++ pages/create-account.vue
@@ -1,15 +1,31 @@
 <template>
    <div class="card text-black flex justify-center h-[30rem] w-[35rem]">
-      <h1 class="card-title">Create Account</h1>
+      <h1 class="card-title mb-5">Create Account</h1>
       <div class="card card-side justify-between">
          <div>
-            <label class="label-text text-black mt-4">First Name<br></label>
-            <input type="text" class="input input-sm border-primary_green bg-primary_white rounded w-fit">
+            <label class="label-text text-black mt-4 font-bold">First Name<br></label>
+            <input type="text" class="input input-sm border-dark_green bg-primary_white rounded w-64">
          </div>
          <div>
-            <label class="label-text text-black mt-4">Last Name<br></label>
-            <input type="text" class="input input-sm border-primary_green bg-primary_white rounded w-full">
+            <label class="label-text text-black mt-4 font-bold">Last Name<br></label>
+            <input type="text" class="input input-sm border-dark_green bg-primary_white rounded w-64">
          </div>
+      </div>
+      <div class="mt-5">
+         <label class="label-text text-black mt-4 font-bold">Email<br></label>
+         <input type="email" class="input input-sm border-dark_green bg-primary_white rounded w-full">
+      </div>
+      <div class="mt-5">
+         <label class="label-text text-black mt-4 font-bold">Password<br></label>
+         <input type="password" class="input input-sm border-dark_green bg-primary_white rounded w-full">
+      </div>
+      <div class="mt-5">
+         <label class="label-text text-black mt-4 font-bold">Confirm Password<br></label>
+         <input type="password" class="input input-sm border-dark_green bg-primary_white rounded w-full">
+      </div>
+      <div class="self-end mt-5">
+         <button class="btn btn-ghost rounded-full btn-sm bg-dark_gray text-white mr-1 px-5">Cancel</button>
+         <button class="btn btn-ghost rounded-full btn-sm bg-button_green text-white m-1">Continue</button>
       </div>
    </div>
 </template>
