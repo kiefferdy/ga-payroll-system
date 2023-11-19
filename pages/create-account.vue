478e50b58b59425efdb7d72c71353a3da71a9cba
IceExconde <isiah_exconde@dlsu.edu.ph>
2023-11-19 20:30:07 +0800

    Update create-account.vue

--- pages/create-account.vue
+++ pages/create-account.vue
@@ -17,18 +17,30 @@
       </div>
       <div class="mt-5">
          <label class="label-text text-black mt-4 font-bold">Password<br></label>
-         <input type="password" class="input input-sm border-dark_green bg-primary_white rounded w-full">
+         <input type="text" class="input input-sm border-dark_green bg-primary_white rounded w-full">
       </div>
       <div class="mt-5">
          <label class="label-text text-black mt-4 font-bold">Confirm Password<br></label>
          <input type="password" class="input input-sm border-dark_green bg-primary_white rounded w-full">
       </div>
       <div class="mt-5">
-         <input type="checkbox"> Needs OTP to Login?
+         <input type="checkbox" class="text-black"> Needs OTP to Login?
       </div>
       <div class="self-end">
          <button class="btn btn-ghost rounded-full btn-sm bg-dark_gray text-white ml-10 mr-1 px-5">Cancel</button>
          <button class="btn btn-ghost rounded-full btn-sm bg-button_green text-white m-1">Conitnue</button>
       </div>
+      <div v-if="wrong" class="mt-2 self-center error">Email is Invalid</div>
+      <div v-if="wrong" class="mt-2 self-center error">Passwords do not match</div>
+      <div v-if="wrong" class="mt-2 self-center error">Please fill all fields before proceeding</div>
    </div>
 </template>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
+
+<script>
+</script>
